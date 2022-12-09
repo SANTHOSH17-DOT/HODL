a5aa8af0ee8049fc476cf1d3c7ff92dc0b10128d
SANTHOSH17-DOT <santhoshm12052003@gmail.com>
2022-12-09 17:19:36 +0530

    Improved: withdraw container UI Use ethers to get deposit amount and hodl time

--- src/components/withdrawContainer/WithdrawContainer.js
+++ src/components/withdrawContainer/WithdrawContainer.js
@@ -4,7 +4,7 @@ import React from "react";
 import { useContractWrite, usePrepareContractWrite } from "wagmi";
 import { CONTRACT_ADDRESS } from "../../config";
 import Hodl from "../../smart-contract/build/contracts/Hodl.json";
-
+import styles from "./style.module.css";
 export const WithdrawContainer = ({ depositAmt, hodlTime }) => {
   const { config } = usePrepareContractWrite({
     address: CONTRACT_ADDRESS,
@@ -13,13 +13,21 @@ export const WithdrawContainer = ({ depositAmt, hodlTime }) => {
   });
   const { data, isLoading, isSuccess, write } = useContractWrite(config);
   return (
-    <>
-      <p> Amount deposited: {depositAmt / 1e18} ETH</p>
-      <p> Freeze time: {new Date(hodlTime * 1000).toString()} </p>
-      <button onClick={() => write?.()} className="btn">
+    <div className={styles.withdraw_container}>
+      <p>
+        Amount deposited <b>{depositAmt} ETH</b>
+      </p>
+      <p>
         {" "}
-        Withdraw{" "}
+        Freeze time <b>{new Date(hodlTime * 1000).toString()}</b>{" "}
+      </p>
+      <button
+        disabled={hodlTime * 1000 > new Date()}
+        onClick={() => write?.()}
+        className={styles.withdraw_btn}
+      >
+        Withdraw
       </button>
-    </>
+    </div>
   );
 };
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -7,22 +7,54 @@ import { useContractRead } from "wagmi";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import DepositContainer from "../../components/depositContainer";
 import WithdrawContainer from "../../components/withdrawContainer";
+import { useState } from "react";
+import { ethers } from "ethers";
+import { useEffect } from "react";
 export const Main = () => {
-  const { data: hodlTime } = useContractRead({
-    address: CONTRACT_ADDRESS,
-    abi: Hodl.abi,
-    method: "getHodlTime",
-  });
-  const {
-    data: depositAmt,
-    isSuccess,
-    isLoading,
-  } = useContractRead({
-    address: CONTRACT_ADDRESS,
-    abi: Hodl.abi,
-    method: "getDepositAmt",
-  });
-
+  // const { data: hodlTime } = useContractRead({
+  //   address: CONTRACT_ADDRESS,
+  //   abi: Hodl.abi,
+  //   method: "getHodlTime",
+  // });
+  // const {
+  //   data: depositAmt,
+  //   isSuccess,
+  //   isLoading,
+  // } = useContractRead({
+  //   address: CONTRACT_ADDRESS,
+  //   abi: Hodl.abi,
+  //   method: "getDepositAmt",
+  // });
+  const [depositAmt, setDepositAmt] = useState();
+  const [hodlTime, setHodlTime] = useState();
+  const getHodlTime = async () => {
+    // const infuraProvider = new ethers.providers.InfuraProvider(
+    //   "goerli",
+    //   process.env.REACT_APP_INFURA_APIKEY
+    // );
+    // const signer = new ethers.Wallet(
+    //   process.env.REACT_APP_PRIVATE_KEY,
+    //   infuraProvider
+    // );
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, Hodl.abi, signer);
+    const hodlTime = await contract.getHodlTime();
+    setHodlTime(hodlTime.toNumber());
+  };
+  const getDepositAmt = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, Hodl.abi, signer);
+    const depositAmt = await contract.getDepositAmt();
+    setDepositAmt(depositAmt.toNumber() / 1e18);
+  };
+  useEffect(() => {
+    getHodlTime();
+    getDepositAmt();
+  }, []);
   return (
     <div className={styles.main_container}>
       <h1 className={styles.title}> HODL </h1>
